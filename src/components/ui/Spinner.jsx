5146440d0901aa878cda0a5bdaa2619cd435db4c
Gourav Chandravanshi <121160526+Gouravchandravanshi@users.noreply.github.com>
2026-09-25 19:32:48 +0530

    Delete src directory

--- src/components/ui/Spinner.jsx
+++
@@ -1,6 +0,0 @@
-export default function Spinner({ size = "md" }) {
-  const s = { sm: "w-4 h-4", md: "w-8 h-8", lg: "w-12 h-12" }[size];
-  return (
-    <div className={`${s} border-2 border-forest-200 border-t-forest-600 rounded-full animate-spin`} />
-  );
-}
